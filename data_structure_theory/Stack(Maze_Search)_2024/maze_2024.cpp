d56380a629e10bcca8ecbdab61d3a22ddb327d81
izumi-hyun <38989711+izumi-hyun@users.noreply.github.com>
2024-05-08 20:18:14 +0900

    Update maze_2024.cpp

--- data_structure_theory/Stack(Maze_Search)_2024/maze_2024.cpp
+++ data_structure_theory/Stack(Maze_Search)_2024/maze_2024.cpp
@@ -5,214 +5,251 @@
 
 #define m 14
 #define p 14
-#define Max_dir 4
-#define Timestep 1000
-#define MAX_STACKSIZE 100
+#define Max_dir 4 // 총 가능한 이동방향의 수
 
-typedef struct AAoff {
-	short int vert;
-	short int horiz;
+typedef struct Aoff {
+    short int vert;
+    short int horiz;
 } offsets;
 
-typedef struct St_element {
-	short int row;
-	short int col;
-	short int dir;
+typedef struct St_element { //스택에 저장할 데이터
+    short int row;
+    short int col;
+    short int dir;
 } ty_element;
-ty_element stack[MAX_STACKSIZE];
-ty_element reverse_stack[MAX_STACKSIZE];
+
 typedef struct ListNode* listPointer;
-typedef struct ListNode {
-	ty_element data;
-	listPointer link;
+typedef struct ListNode { //연결스택의 노드
+    ty_element data;
+    listPointer link;
 } listNode;
 
-offsets move[Max_dir] = { {0,1}, {0, -1}, {1, 0}, {-1, 0} }; //E, W, S, N
+offsets move[Max_dir]{ {0,1}, {0,-1},{1,0}, {-1,0} }; //E W S N
 
-int maze[m + 2][p + 2];
-int mark[m + 2][p + 2];
+int maze[m + 2][p + 2]; // maze 행렬. 전역변수로 선언
+int mark[m + 2][p + 2]; // mark 행렬
 
 void gotoxy(int x, int y)
 {
-	COORD Pos = { x ,y };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+    COORD Pos = { x ,y };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 }
 
 void push(listPointer* top, ty_element pos)
 {
-	listPointer temp = (listPointer)malloc(sizeof(listNode));
-	temp->data.row = pos.row;
-	temp->data.col = pos.col;
-	temp->data.dir = pos.dir;
-	temp->link = *top;
-	*top = temp;
+    listPointer temp = (listPointer)malloc(sizeof(listNode));
+    temp->data.row = pos.row;
+    temp->data.col = pos.col;
+    temp->data.dir = pos.dir;
+    temp->link = *top;
+    *top = temp;
 }
 ty_element pop(listPointer* top)
 {
-	listPointer temp = *top;
-	ty_element pdata = temp->data;
-	*top = temp->link;
-	free(temp);
-	return pdata;
+    listPointer temp = *top;
+    ty_element pdata = temp->data;
+    *top = temp->link;
+    free(temp);
+    return pdata;
 }
 void reverse(listPointer* top)
 {
-	listPointer temp, curr, next;
+    listPointer temp, curr, next;
 
-	temp = NULL;
-	curr = *top;
-	do {
-		next = curr->link;
-		curr->link = temp;
-		temp = curr;
-		curr = next;
-	} while (curr);
-	*top = temp;
+    temp = NULL;
+    curr = *top;
+    do {
+        next = curr->link;
+        curr->link = temp;
+        temp = curr;
+        curr = next;
+    } while (curr);
+    *top = temp;
 }
 
 int path(int sy, int sx, int dy, int dx)
 {
-	listPointer top = NULL;
-	listPointer rtop = NULL;
-	int i = 0, j = 0, row = 0, col = 0, nextRow = 0, nextCol = 0, dir = 0, basex = 0, basey = 0, ocount = 0;
-	int found = 0, num_bktrack = 0, path_length = 0;
-	int EXIT_ROW = dy, EXIT_COL = dx;
-	ty_element position;
-	if (maze[sy][sx] == 1 || maze[dy][dx] == 1)
-		if (maze[sy][sx] == 1)
-		{
-			printf("입력오류 : 출발점이나 목표점이 막힌 셀입니다.\n");
-			return 0;
-		}
-	if (sy == dy && sx == dx)
-	{
-		printf("입력오류 : 출발점과 목표점이 같습니다.\n");
-		return 0;
-	}
+    listPointer top = NULL;
+    listPointer rtop = NULL;
+    int i = 0, j = 0, row = 0, col = 0, nextRow = 0, nextCol = 0, dir = 0, basex = 0, basey = 0, ocount = 0;
+    int found = 0, num_bktrack = 0, path_length = 0;
+    int EXIT_ROW = dy, EXIT_COL = dx;
+    ty_element position;
+    int stop = 0;
+    if (maze[sy][sx] == 1 || maze[dy][dx] == 1)
+    {
+        printf("입력오류 : 출발점이나 목표점이 막힌 셀입니다.\n");
+        return 0;
+    }
+    if (sy == dy && sx == dx)
+    {
+        printf("입력오류 : 출발점과 목표점이 같습니다.\n");
+        return 0;
+    }
 
-	position.row = sy;
-	position.col = sx;
-	position.dir = 0;
-	push(&top, position);
+    // 먼지 미로를 화면에 그린다.
+    CONSOLE_SCREEN_BUFFER_INFO presentCur;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &presentCur);
+    basex = presentCur.dwCursorPosition.X;
+    basey = presentCur.dwCursorPosition.Y;
 
-	for (i = 0; i < m; i++)
-	{
-		for (j = 0; j < p; j++)
-		{
-			mark[1 + i][1 + j] = 0;
-		}
-	}
+    for (i = 0; i < m + 2; i++)
+    {
+        for (j = 0; j < p + 2; j++)
+        {
+            gotoxy(j + basex, i + basey);
+            printf("%1d", maze[i][j]);
+        }
+    }
 
-	while (!found && top)
-	{
-		position = pop(&top);
-		row = position.row;
-		col = position.col;
-		dir = position.dir;
+    position.row = sy;
+    position.col = sx;
+    position.dir = 0;
+    push(&top, position);
 
-		while (dir < Max_dir && !found)
-		{
-			nextRow = row + move[dir].vert;
-			nextCol = col + move[dir].horiz;
+    for (i = 0; i < m + 2; i++)
+    {
+        for (j = 0; j < p + 2; j++)
+        {
+            mark[1 + i][1 + j] = 0;
+        }
+    }
 
-			if (row == sy && col == sx)
-			{
-				mark[row][col] = 1;
-			}
-			else
-			{
-			}
+    char ch;
+    for (i = 0; i < m + 2; i++) {
+        for (j = 0; j < p + 2; j++) {
+            gotoxy(j + basex, i + basey);
+            if (maze[i][j] == 1)
+                ch = '1';
+            else if (maze[i][j] == 2)
+                ch = '2';
+            else
+                ch = '0';
+            printf("%c", ch);
+        }
+    }
+    while (!found && top)
+    {
+        position = pop(&top);
+        row = position.row;
+        col = position.col;
+        dir = position.dir;
 
-			if (nextRow == EXIT_ROW && nextCol == EXIT_COL)
-			{
-				mark[row][col] = 1;
-				position.row = row; position.col = col;
-				position.dir = ++dir;
-				push(&top, position);
-				found = TRUE;
-			}
-			else if (!maze[nextRow][nextCol] && !mark[nextRow][nextCol])
-			{
+        while (dir < Max_dir && !found)
+        {
+            nextRow = row + move[dir].vert;
+            nextCol = col + move[dir].horiz;
 
-				position.row = row;
-				position.col = col;
-				position.dir = ++dir;
-				push(&top, position);
-				mark[nextRow][nextCol] = 1;
-				row = nextRow;
-				col = nextCol;
-				dir = 0;
-			}
-			else if (maze[nextRow][nextCol] == 2)
-			{
-				printf("2를 만나서 멈췄습니다.");
-				return 1;
-			}
-			else
-			{
-				++dir;
-				if (dir == Max_dir)
-				{
-					mark[row][col] = 1;
-					num_bktrack++;
-				}
-			}
-		}
-	}
+            gotoxy(col + basex, row + basey);
+            if (row == sy && col == sx)
+            {
+                printf(">");
+                mark[row][col] = 1;
+            }
+            else
+            {
+                printf("*");
+            }
 
-	if (found)
-	{
-		printf("\n찾은 경로(row,col):\n");
-		path_length = 0;
-		reverse(&top);
-		while (top)
-		{
+            if (nextRow == EXIT_ROW && nextCol == EXIT_COL)
+            {
+                mark[row][col] = 1;
+                gotoxy(nextCol + basex, nextRow + basey);
+                printf("<");
+                position.row = row; position.col = col;
+                position.dir = ++dir;
+                push(&top, position);
+                found = TRUE;
+            }
+            else if (!maze[nextRow][nextCol] && !mark[nextRow][nextCol])
+            {
 
-			position = pop(&top);
-			printf("[%2d,%2d] ", position.row, position.col);
-			++path_length;
-		}
-		printf("[%2d,%2d]\n", dy, dx);
-		++path_length;
-		printf("경로길이 = %d, 백트래킹수= %d", path_length, num_bktrack);
-	}
-	else
-	{
-		printf("경로없음\n");
-	}
-
+                position.row = row;
+                position.col = col;
+                position.dir = dir;
+                push(&top, position);
+                mark[nextRow][nextCol] = 1;
+                row = nextRow;
+                col = nextCol;
+                dir = 0;
+            }
+            else if (maze[nextRow][nextCol] == 2)
+            {
+                mark[row][col] = 1;
+                gotoxy(nextCol + basex, nextRow + basey);
+                printf("!");
+                position.row = row; position.col = col;
+                position.dir = ++dir;
+                push(&top, position);
+                stop = 1;
+                break;
+            }
+            else
+            {
+                ++dir;
+                if (dir >= Max_dir)
+                {
+                    gotoxy(col + basex, row + basey);
+                    printf("$");
+                    mark[row][col] = 1;
+                    num_bktrack++;
+                }
+            }
+        }
+        if (stop == 1)
+        {
+            break;
+        }
+    }
+    gotoxy(col + basex+4, row + basey+4);
+    if (stop == 1)
+    {
+        printf("\n장애물 발견으로 중단");
+    }
+    printf("\n찾은 경로(row,col):\n");
+    path_length = 0;
+    reverse(&top);
+    while (top)
+    {
+        position = pop(&top);
+        printf("[%2d,%2d] ", position.row, position.col);
+        ++path_length;
+    }
+    ++path_length;
+    printf("[%2d,%2d]", nextRow, nextCol);
+    printf("\n경로길이 = %d, 백트래킹수= %d", path_length, num_bktrack);
 }
 
 
 int main()
 {
-	// (fopen, fscanf 이용).
-	FILE* fp = NULL;
-	fp = fopen("maze.txt", "r");
-	if (!fp)
-	{
-		printf("maze_14_2.txt not found");
-		return 1;
-	}
-	int starty; int startx;
-	int endy; int endx;
-	int res;
-	int i = 0;
-	while (!feof(fp))
-	{
-		for (int j = 0; j < p + 2; j++)
-		{
-			fscanf(fp, "%d", &maze[i][j]);
-		}
-		i++;
-	}
+    // (fopen, fscanf 이용).
+    FILE* fp = NULL;
+    fp = fopen("maze.txt", "r");
+    if (!fp)
+    {
+        printf("maze_14_2.txt not found");
+        return 1;
+    }
+    int starty; int startx;
+    int endy; int endx;
+    int res;
+    int i = 0;
+    while (!feof(fp))
+    {
+        for (int j = 0; j < p + 2; j++)
+        {
+            fscanf(fp, "%d", &maze[i][j]);
+        }
+        i++;
+    }
+    while (TRUE) {
 
-	while (TRUE) {
 
-		printf("\n");
-		printf("출발점, 목표점은?(4개의 정수; 종료는 Control+DEnter): ");
-		res = scanf("%d%d%d%d", &starty, &startx, &endy, &endx);
-		if (res != 4) break;
-		path(starty, startx, endy, endx);
-	}
+        printf("\n");
+        printf("출발점, 목표점은?(4개의 정수; 종료는 Control+DEnter): ");
+        res = scanf("%d%d%d%d", &starty, &startx, &endy, &endx);
+        if (res != 4) break;
+        path(starty, startx, endy, endx);
+    }
 }
